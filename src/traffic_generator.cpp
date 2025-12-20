9ca52e3a05bf9c7364bff91e4e6dc4e60b901a8f
adhikariaarjan7-del <adhikariaarjan7@gmail.com>
2025-12-20 19:51:50 +0545

    Created basic generator program

--- src/traffic_generator.cpp
+++ src/traffic_generator.cpp
@@ -5,3 +5,10 @@ int main() {
     cout << "Traffic generator starting..." << endl;
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Traffic generator starting..." << endl;
+    return 0;
+}
