e4e58c8a7fec68907985841eea0d0173271f6e41
adhikariaarjan7-del <adhikariaarjan7@gmail.com>
2025-12-22 21:41:50 +0545

    Added commet and maintain  readability code

--- src/traffic_generator.cpp
+++ src/traffic_generator.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+/*
+ This function creates a random vehicle ID
+ Example: AB3
+*/
 string generateVehicleID() {
     string id = "";
     id += char('A' + rand() % 26);
@@ -13,31 +17,46 @@ string generateVehicleID() {
     id += char('0' + rand() % 10);
     return id;
 }
+
+/*
+ This function selects random road and lane
+ Only A, B, C, D roads
+ Example: AL1, BL2
+*/
 string generateLane() {
-    char roads[] = {'A','B','C','D'};
-    int lane = rand() % 3 + 1;
+    char roads[] = {'A', 'B', 'C', 'D'};
+    int lane = rand() % 3 + 1; // Lane 1 to 3
 
     string laneID = "";
     laneID += roads[rand() % 4];
     laneID += "L";
     laneID += char('0' + lane);
+
     return laneID;
 }
 
-
 int main() {
-   ofstream file("data/vehicles.data", ios::app);
-if(!file)
-{
-    cout << "Error opening file!" << endl;
-    while(true) {
-    string v = generateVehicleID();
-    string l = generateLane();
-    file << v << ":" << l << endl;
-    cout << "Generated " << v << " in " << l << endl;
-    sleep(1);
-}
+    srand((unsigned int)time(NULL));
 
-    return 1;
-}
+    ofstream file("data/vehicles.data", ios::app);
 
+    if (!file) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    cout << "Traffic generator started..." << endl;
+
+    while (true) {
+        string vehicle = generateVehicleID();
+        string lane = generateLane();
+
+        file << vehicle << ":" << lane << endl;
+        cout << "Generated: " << vehicle << " in " << lane << endl;
+
+        sleep(1); // wait 1 second
+    }
+
+    file.close();
+    return 0;
+}
