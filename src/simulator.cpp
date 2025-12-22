f8036f4d614fa6da7d4be280ad153a1bdb38efc6
adhikariaarjan7-del <adhikariaarjan7@gmail.com>
2025-12-22 21:29:09 +0545

    Load vehicles from file into queues

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -8,6 +9,22 @@ queue<string> AL1, AL2, AL3;
 queue<string> BL1, BL2, BL3;
 queue<string> CL1, CL2, CL3;
 queue<string> DL1, DL2, DL3;
+
+
+
+void loadVehicles()
+ {
+    ifstream file("data/vehicles.data");
+    string line;
+    while(getline(file, line)) {
+        string lane = line.substr(line.find(":") + 1);
+        if(lane=="AL2") AL2.push(line);
+        else if(lane=="BL2") BL2.push(line);
+        else if(lane=="CL2") CL2.push(line);
+        else if(lane=="DL2") DL2.push(line);
+    }
+}
+
 
 
 int main() {
