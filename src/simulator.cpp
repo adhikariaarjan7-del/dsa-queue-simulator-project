a622397938b280df26dd66c3d523acc2f08012fa
adhikariaarjan7-del <adhikariaarjan7@gmail.com>
2025-12-23 18:54:01 +0545

    Loaded vehicles from file

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -8,6 +8,15 @@ queue<string> AL1, AL2, AL3;
 queue<string> BL1, BL2, BL3;
 queue<string> CL1, CL2, CL3;
 queue<string> DL1, DL2, DL3;
+void loadVehicles() {
+    ifstream file("data/vehicles.data");
+    string line;
+    while(getline(file, line)) {
+        string lane = line.substr(line.find(":") + 1);
+        if(lane == "AL2") AL2.push(line);
+    }
+}
+
 
 int main() {
     cout << "Simulator started" << endl;
