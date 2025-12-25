43afa3aa99e0328bb90c1a2d48165eda4b118306
adhikariaarjan7-del <adhikariaarjan7@gmail.com>
2025-12-25 14:29:42 +0545

    Create SDL window for traffic junction

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main() {
-     if (SDL_Init(SDL_INIT_VIDEO) != 0) 
-     {
+    
+    SDL_Window* window = SDL_CreateWindow(
+    "Traffic Junction",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    800, 600,
+    SDL_WINDOW_SHOWN
+);
+
+
+     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         cout << "SDL Init Failed" << endl;
         return 1;
     }
